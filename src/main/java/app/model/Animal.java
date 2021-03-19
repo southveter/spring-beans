0ef0fd5dee623f4651226c5f82fbafc37115f82d
southveter <southveter@gmail.com>
2021-03-19 20:16:44 +0300

    Добавлено решение

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,8 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public abstract class Animal {
+
 }
